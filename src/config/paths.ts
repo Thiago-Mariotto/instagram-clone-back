100ee27ff47fcd93abba1c6c8bae87967cd97990
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-07-14 17:42:08 -0300

    change buckAws to use .env

--- src/config/paths.ts
+++ src/config/paths.ts
@@ -2,5 +2,5 @@ import path from 'path';
 
 export default {
 	tempUploadFolder: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-	bucketAws: 'https://insta-test.s3.amazonaws.com/'
+	bucketAws: process.env.AWS_BUCKET_URL,
 }
