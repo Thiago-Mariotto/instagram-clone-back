7a67cd3841e88f238ed46ddc46f19b22ca706164
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-07-14 19:03:02 -0300

    chore: add another case erros

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -5,7 +5,13 @@ export const errorHandler = (err: Error, req: Request, res: Response, next: Next
 	if (err instanceof Error) {
 		switch (err.message) {
 			case "upload failed": return res.status(503).json({ message: 'Upload temporary unavailable, try again later' });
-			default: return res.status(500).json({ message: 'Internal Server Error' });
+			case "service unavailable": return res.status(503).json({ message: 'Service unavailable, try again later' });
+			case "not found": return res.status(404).json({ message: 'Not found' });
+			case "unauthorized": return res.status(401).json({ message: 'Unauthorized' });
+			case "forbidden": return res.status(403).json({ message: 'Forbidden' });
+			case "bad request": return res.status(400).json({ message: 'Bad request' });
+
+			default: return res.status(500).json({ message: 'Internal server error' });
 		}
 	}
 
