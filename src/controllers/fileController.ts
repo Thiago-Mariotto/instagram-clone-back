df03900829cbc93be5f8b0c6b66988913b92e475
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-07-14 17:44:08 -0300

    feat: add File controller with upload and delete

---
+++ src/controllers/fileController.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import uploadFileService from "../services/uploadFile";
+
+export const uploadFileController = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const { filename } = req.body;
+
+		const uploadedFile = await uploadFileService.uploadFile(filename);
+
+		if (!uploadedFile) throw new Error(`Upload failed`)
+		return res.status(201).send(uploadedFile);
+	} catch (err) {
+		next(err);
+	}
+};
+
+export const deleteFileController = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const { filename } = req.body;
+
+		await uploadFileService.deleteFile(filename);
+		return res.status(204).send('Successfully deleted');
+	} catch (err) {
+		next(err);
+	}
+}
