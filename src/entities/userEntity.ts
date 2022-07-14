e4f14fe6bff1b713c1fc506c0b633e483f37de3a
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-07-14 19:44:18 -0300

    chore: add user entity

--- src/entities/userEntity.ts
+++ src/entities/userEntity.ts
@@ -1,5 +1,7 @@
+import JOI from 'joi';
+
 export type User = {
-	id: string;
+	id: number;
 	name: string;
 	username: string;
 	email: string;
@@ -10,3 +12,12 @@ export type User = {
 
 export type CreateUser = Omit<User, 'id'>;
 export type UpdateUser = Partial<User>;
+
+export const userSchema = JOI.object({
+	name: JOI.string().required().min(4).max(20),
+	username: JOI.string().required().min(4).max(20),
+	email: JOI.string().required().email(),
+	password: JOI.string().required().min(4).max(20),
+	createdAt: JOI.date().required(),
+	updatedAt: JOI.date().required()
+});
