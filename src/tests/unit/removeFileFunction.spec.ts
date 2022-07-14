164b9fc35b702681e2204d6af1461368ae747976
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-07-14 17:49:32 -0300

    chore: change file name

---
+++ src/tests/unit/removeFileFunction.spec.ts
@@ -0,0 +1,42 @@
+import { expect } from 'chai';
+import fs from 'fs';
+import configPaths from '../../config/paths';
+import removeTempFile from '../../utils/deleteTmpFile';
+
+const tempFolderPath = configPaths.tempUploadFolder;
+const correctFilename = 'tempFile.txt';
+const incorrectFilename = 'incorrectFile.txt';
+const tempFilePath = `${tempFolderPath}/${correctFilename}`;
+
+describe('-> Testing the temp file removal function', () => {
+	describe('When the temp file exists', () => {
+
+		beforeEach(() => {
+			fs.writeFileSync(tempFilePath, 'test');
+		});
+
+		afterEach(() => {
+			const fileExists = fs.existsSync(tempFilePath);
+			if (fileExists) {
+				fs.unlinkSync(tempFilePath);
+			}
+		});
+
+		it('The function return true', async () => {
+			const response = await removeTempFile(correctFilename);
+			expect(response).to.be.equal(true);
+		});
+
+		it('The temp file is removed', async () => {
+			await removeTempFile(correctFilename);
+			expect(fs.existsSync(tempFilePath)).to.be.equal(false);
+		});
+	});
+
+	describe('When the temp file not exists', () => {
+		it('The function return false', async () => {
+			const response = await removeTempFile(incorrectFilename);
+			expect(response).to.be.equal(false);
+		});
+	});
+});
