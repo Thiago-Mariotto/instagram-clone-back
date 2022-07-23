73114cee29c315c4c7209e6b88746f49be0a7d02
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-07-23 19:04:47 -0300

    chore: add user route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from 'express';
-import gallery from './galleryRoute';
+import galleryRoute from './galleryRoute';
+import userRoute from './userRoute';
 
 const router = express.Router();
 
-router.use('/gallery', gallery);
-
+router.use('/gallery', galleryRoute);
+router.use('/user', userRoute);
 router.get('/', (req: Request, res: Response) => res.send('Successfully'));
 
 export default router;
